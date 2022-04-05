4c7ec84382a9b83d50537e62af56b01bbb915c8a
Taufik Nugraha <taufik.adinugraha@gmail.com>
2022-04-05 12:57:38 +0700

    fix save model

--- tools.py
+++ tools.py
@@ -144,7 +144,7 @@ class evaluation_callback(Callback):
         
         if self.i%100==0:
           # copy model
-          os.system(f'cp unet.h5 /mydrive/AI/Unet-Chicken')
+          os.system(f'/content/cp unet.h5 /mydrive/AI/Unet-Chicken')
           # test image
           fig, ax = plt.subplots(1, 3, figsize=(18,5))
           for i, image in enumerate(self.files[:3]):
